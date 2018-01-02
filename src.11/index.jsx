087dc142db1e6eb979996539e9561a6942bcbae5
Hugo <hugooliveirahenrique@gmail.com>
2018-01-02 16:29:02 -0200

    Iniciando o contandor com redux

--- src.11/index.jsx
+++ src.11/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import  {combineReducers, createStore} from 'redux'
 import { Provider } from 'react-redux'
 
+import Field from './field'
+import FieldReducer from './fieldReducer'
 // React DOM somente quando precisar renderizar tudo
 
 // Reducer são funcoes 
 const reducers = combineReducers({
-    
+    field: FieldReducer
 })
 
 ReactDOM.render(
